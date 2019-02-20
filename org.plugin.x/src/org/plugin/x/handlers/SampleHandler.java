6d10afc5e5c5c815979782e1bacb640e1e028180
vishalmisra235 <34361144+vishalmisra235@users.noreply.github.com>
2019-02-20 21:49:30 +0530

    Updated file

--- org.plugin.x/src/org/plugin/x/handlers/SampleHandler.java
+++ org.plugin.x/src/org/plugin/x/handlers/SampleHandler.java
@@ -1,5 +1,6 @@
-package org.plugin.x.handlers;
+package org.plugin.facoytool.handlers;
 import java.io.IOException;
+import java.util.ArrayList;
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
@@ -17,6 +18,7 @@ import org.jsoup.*;
 import org.jsoup.nodes.Document;
 
 public class SampleHandler extends AbstractHandler {
+	ArrayList<String> results = new ArrayList<String>();
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
 		try {               
@@ -24,33 +26,63 @@ public class SampleHandler extends AbstractHandler {
 		    if ( part instanceof ITextEditor ) {
 		        final ITextEditor editor = (ITextEditor)part;
 		        IDocumentProvider prov = editor.getDocumentProvider();
-		        IDocument doc = prov.getDocument( editor.getEditorInput() );
+		        
+		   
+				IDocument doc = prov.getDocument( editor.getEditorInput() );
 		        ISelection sel = editor.getSelectionProvider().getSelection();
 		        IWorkbenchWindow window = HandlerUtil.getActiveWorkbenchWindowChecked(event);
-			   
+		        int offset = doc.getLineOffset(doc.getNumberOfLines()-1);
+		        
 		        if ( sel instanceof TextSelection ) {
 		            final TextSelection textSel = (TextSelection)sel;
 		            String txt=textSel.getText();
 		            MessageDialog.openInformation(
 							window.getShell(),
 							"Selected text",
-							txt);
+							"Your selected text is\n\n" + txt);
 		            
 		            scrap(txt);
 		        }
+		        
+		        for(String x: results)
+		        {
+		        	MessageDialog dialog = new MessageDialog(window.getShell(),"Codes",null,x,MessageDialog.INFORMATION, new String[] {"Next Code","Close","Copy"},0);
+		        	
+		        	int a =dialog.open();
+		        	System.out.println(a);
+		        	
+		        	if(a==0)
+		        	{
+		        		continue;
+		        	}
+		        	else if(a==2)
+		        	{
+		        		doc.replace(offset, 0, x+"\n");
+		        		break;
+		        	}
+		        	else
+		        	{
+		        		break;
+		        	}
+		        }
+		        
 		    }
-		} catch( Exception ex ){
+		} 
+		
+		catch( Exception ex ){
 		    ex.printStackTrace();
 		}
 		return null;
 	}
+	
+	
 	private void scrap(String txt) {
 		Document doc = null;
 		try {
-			 doc = Jsoup.connect("http://code-search.uni.lu/facoy").data("q",txt).get();
-			  // System.out.println(doc.outerHtml());
+			 doc = Jsoup.connect("http://code-search.uni.lu/facoy").data("q",txt).timeout(10 * 1000).get();
 			 for (org.jsoup.nodes.Element item : doc.select("div.snippet_item table.highlighttable tr")) {
 				String data_item=item.select(".code").text();
+				results.add(data_item);
 				System.out.println(data_item);
 			}
 			 
@@ -59,4 +91,5 @@ public class SampleHandler extends AbstractHandler {
 			e.printStackTrace();
 		}
 	}
+	
 }
